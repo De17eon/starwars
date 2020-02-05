c4acff491228861a533a57daf467e59be61641f0
Leonid <de17eon@gmail.com>
2020-02-05 20:06:55 +0800

    first commit

---
+++ precache-manifest.df7e924432b4e003ca69fdd97d3fc8d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d542a9356bc985a35412",
+    "url": "/static/css/main.7e0a022d.chunk.css"
+  },
+  {
+    "revision": "d542a9356bc985a35412",
+    "url": "/static/js/main.d542a935.chunk.js"
+  },
+  {
+    "revision": "518451d5f78da16fae19",
+    "url": "/static/js/1.518451d5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "995df847e5338f43e2861bc1579d4b8c",
+    "url": "/static/media/e-droid2.995df847.png"
+  },
+  {
+    "revision": "42d3164b422af728fca0ebd500b08305",
+    "url": "/index.html"
+  }
+];
